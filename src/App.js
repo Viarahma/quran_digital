c91c13911082e50eed61b452ba2da9ec7c94ba5a
ekasintaap <72989524+ekasintaap@users.noreply.github.com>
2022-05-20 21:30:26 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Route, Routes} from "react-router-dom"
+import NavbarCom from "./component/navbar";
+import FooterCom from "./component/footer";
+import HomeCOm from "./page/home";
+import SurahCom from "./page/surah";
+import InfoCOm from "./page/info";
+import JuzCOm from "./page/juz";
+import AboutCom from "./page/about";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarCom/>
+      <Routes>
+        <Route path="/" element={<HomeCOm/>}/>
+        <Route path="surah/:id" element={<SurahCom/>}/>
+        <Route path="juz/:id" element={<JuzCOm/>}/>
+        <Route path="info/:id" element={<InfoCOm/>}/>
+        <Route path="about" element={<AboutCom/>}/>
+          <Route path="*" element={<HomeCOm/>}/>
+      </Routes>
+      <FooterCom/>
     </div>
   );
 }
